c5daa680eb6f504b319c903853df2bd388f0efc4
Zin Min Htet <zinminht3t@gmail.com>
2018-03-22 17:58:17 +0800

    Facility Design

---
+++ SportsFacilityManagementSystem/ucFacilities.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace SportsFacilityManagementSystem
+{
+    partial class ucFacilities
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtKeyword = new System.Windows.Forms.TextBox();
+            this.cmbSearchBy = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.cmbRates = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbNoofFacilities = new System.Windows.Forms.ComboBox();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(368, 167);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(187, 47);
+            this.btnSearch.TabIndex = 29;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // txtKeyword
+            // 
+            this.txtKeyword.Location = new System.Drawing.Point(488, 96);
+            this.txtKeyword.Name = "txtKeyword";
+            this.txtKeyword.Size = new System.Drawing.Size(414, 31);
+            this.txtKeyword.TabIndex = 28;
+            // 
+            // cmbSearchBy
+            // 
+            this.cmbSearchBy.FormattingEnabled = true;
+            this.cmbSearchBy.Location = new System.Drawing.Point(85, 96);
+            this.cmbSearchBy.Name = "cmbSearchBy";
+            this.cmbSearchBy.Size = new System.Drawing.Size(341, 33);
+            this.cmbSearchBy.TabIndex = 27;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(79, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 25);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "Search by : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(80, 343);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 25);
+            this.label2.TabIndex = 30;
+            this.label2.Text = "Facility : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(80, 439);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 25);
+            this.label3.TabIndex = 31;
+            this.label3.Text = "Rates : ";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(297, 337);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(414, 31);
+            this.txtName.TabIndex = 32;
+            // 
+            // cmbRates
+            // 
+            this.cmbRates.FormattingEnabled = true;
+            this.cmbRates.Location = new System.Drawing.Point(297, 439);
+            this.cmbRates.Name = "cmbRates";
+            this.cmbRates.Size = new System.Drawing.Size(422, 33);
+            this.cmbRates.TabIndex = 33;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(80, 527);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(172, 25);
+            this.label4.TabIndex = 34;
+            this.label4.Text = "No of Facilities : ";
+            // 
+            // cmbNoofFacilities
+            // 
+            this.cmbNoofFacilities.FormattingEnabled = true;
+            this.cmbNoofFacilities.Location = new System.Drawing.Point(297, 519);
+            this.cmbNoofFacilities.Name = "cmbNoofFacilities";
+            this.cmbNoofFacilities.Size = new System.Drawing.Size(422, 33);
+            this.cmbNoofFacilities.TabIndex = 35;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(532, 707);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(179, 58);
+            this.btnEdit.TabIndex = 36;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // ucFacilities
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.cmbNoofFacilities);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cmbRates);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtKeyword);
+            this.Controls.Add(this.cmbSearchBy);
+            this.Controls.Add(this.label1);
+            this.Name = "ucFacilities";
+            this.Size = new System.Drawing.Size(944, 863);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtKeyword;
+        private System.Windows.Forms.ComboBox cmbSearchBy;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.ComboBox cmbRates;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbNoofFacilities;
+        private System.Windows.Forms.Button btnEdit;
+    }
+}
