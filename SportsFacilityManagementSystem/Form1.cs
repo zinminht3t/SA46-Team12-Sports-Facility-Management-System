d802dc8d56e3f8784c63db58a33328fc172f15f7
Zin Min Htet <zinminht3t@gmail.com>
2018-03-22 19:11:15 +0800

    Form1 test

---
+++ SportsFacilityManagementSystem/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SportsFacilityManagementSystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SportsFacilitiesEntities ctx = new SportsFacilitiesEntities();
+
+
+            DataSet1 ds = new DataSet1();
+
+            DataSet1TableAdapters.MembersTableAdapter da = new DataSet1TableAdapters.MembersTableAdapter();
+
+            da.Fill(ds.Members);
+            MemberListing cr = new MemberListing();
+            cr.SetDataSource(ds);
+
+            crystalReportViewer1.ReportSource = cr;
+            
+
+        }
+    }
+}
--- SportsFacilityManagementSystem/Program.cs
+++ SportsFacilityManagementSystem/Program.cs
@@ -16,7 +16,7 @@ namespace SportsFacilityManagementSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmMain());
+            Application.Run(new Form1());
         }
     }
 }
