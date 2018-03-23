c180cf368a9aa826a9b7110b06a2f93325dc4763
Jayden <wwj.jayden@gmail.com>
2018-03-23 10:36:46 +0800

    Logo and new uc

--- SportsFacilityManagementSystem/MainForm.Designer.cs
+++ SportsFacilityManagementSystem/MainForm.Designer.cs
@@ -28,14 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
             this.pnlSidebar = new System.Windows.Forms.Panel();
-            this.pbLogo = new System.Windows.Forms.PictureBox();
             this.pnlSidetab = new System.Windows.Forms.Panel();
-            this.btnLogout = new System.Windows.Forms.Button();
-            this.btnBooking = new System.Windows.Forms.Button();
-            this.btnMembers = new System.Windows.Forms.Button();
-            this.btnFacilities = new System.Windows.Forms.Button();
-            this.btnReports = new System.Windows.Forms.Button();
             this.btnSearchMembers = new System.Windows.Forms.Button();
             this.btnAddMembers = new System.Windows.Forms.Button();
             this.btnAddFacilities = new System.Windows.Forms.Button();
@@ -43,6 +38,12 @@
             this.lblWelcome = new System.Windows.Forms.Label();
             this.btnPasttransactions = new System.Windows.Forms.Button();
             this.btnUpcoming = new System.Windows.Forms.Button();
+            this.pbLogo = new System.Windows.Forms.PictureBox();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.btnBooking = new System.Windows.Forms.Button();
+            this.btnMembers = new System.Windows.Forms.Button();
+            this.btnFacilities = new System.Windows.Forms.Button();
+            this.btnReports = new System.Windows.Forms.Button();
             this.pnlSidebar.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbLogo)).BeginInit();
             this.SuspendLayout();
@@ -63,15 +64,6 @@
             this.pnlSidebar.Size = new System.Drawing.Size(229, 553);
             this.pnlSidebar.TabIndex = 6;
             // 
-            // pbLogo
-            // 
-            this.pbLogo.Location = new System.Drawing.Point(-1, 0);
-            this.pbLogo.Name = "pbLogo";
-            this.pbLogo.Size = new System.Drawing.Size(230, 160);
-            this.pbLogo.TabIndex = 15;
-            this.pbLogo.TabStop = false;
-            this.pbLogo.MouseEnter += new System.EventHandler(this.pbLogo_MouseEnter);
-            // 
             // pnlSidetab
             // 
             this.pnlSidetab.BackColor = System.Drawing.Color.DeepSkyBlue;
@@ -79,97 +71,6 @@
             this.pnlSidetab.Name = "pnlSidetab";
             this.pnlSidetab.Size = new System.Drawing.Size(17, 76);
             this.pnlSidetab.TabIndex = 7;
-            // 
-            // btnLogout
-            // 
-            this.btnLogout.BackColor = System.Drawing.Color.LightSkyBlue;
-            this.btnLogout.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.btnLogout.FlatAppearance.BorderSize = 0;
-            this.btnLogout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnLogout.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLogout.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnLogout.Image = global::SportsFacilityManagementSystem.Properties.Resources.Logout;
-            this.btnLogout.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnLogout.Location = new System.Drawing.Point(13, 467);
-            this.btnLogout.Name = "btnLogout";
-            this.btnLogout.Size = new System.Drawing.Size(215, 76);
-            this.btnLogout.TabIndex = 10;
-            this.btnLogout.Text = "Logout";
-            this.btnLogout.UseVisualStyleBackColor = false;
-            this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
-            this.btnLogout.MouseEnter += new System.EventHandler(this.btnLogout_MouseEnter);
-            // 
-            // btnBooking
-            // 
-            this.btnBooking.BackColor = System.Drawing.Color.LightSkyBlue;
-            this.btnBooking.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.btnBooking.FlatAppearance.BorderSize = 0;
-            this.btnBooking.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnBooking.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnBooking.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnBooking.Image = global::SportsFacilityManagementSystem.Properties.Resources.Booking;
-            this.btnBooking.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnBooking.Location = new System.Drawing.Point(13, 160);
-            this.btnBooking.Name = "btnBooking";
-            this.btnBooking.Size = new System.Drawing.Size(215, 76);
-            this.btnBooking.TabIndex = 1;
-            this.btnBooking.Text = "Bookings";
-            this.btnBooking.UseVisualStyleBackColor = false;
-            this.btnBooking.MouseEnter += new System.EventHandler(this.btnBooking_MouseEnter);
-            // 
-            // btnMembers
-            // 
-            this.btnMembers.BackColor = System.Drawing.Color.LightSkyBlue;
-            this.btnMembers.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.btnMembers.FlatAppearance.BorderSize = 0;
-            this.btnMembers.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnMembers.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnMembers.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnMembers.Image = global::SportsFacilityManagementSystem.Properties.Resources.Members;
-            this.btnMembers.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnMembers.Location = new System.Drawing.Point(13, 236);
-            this.btnMembers.Name = "btnMembers";
-            this.btnMembers.Size = new System.Drawing.Size(215, 76);
-            this.btnMembers.TabIndex = 7;
-            this.btnMembers.Text = "Members";
-            this.btnMembers.UseVisualStyleBackColor = false;
-            this.btnMembers.MouseEnter += new System.EventHandler(this.btnMembers_MouseEnter);
-            // 
-            // btnFacilities
-            // 
-            this.btnFacilities.BackColor = System.Drawing.Color.LightSkyBlue;
-            this.btnFacilities.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.btnFacilities.FlatAppearance.BorderSize = 0;
-            this.btnFacilities.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnFacilities.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnFacilities.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnFacilities.Image = global::SportsFacilityManagementSystem.Properties.Resources.Stadium;
-            this.btnFacilities.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnFacilities.Location = new System.Drawing.Point(13, 312);
-            this.btnFacilities.Name = "btnFacilities";
-            this.btnFacilities.Size = new System.Drawing.Size(215, 76);
-            this.btnFacilities.TabIndex = 9;
-            this.btnFacilities.Text = "Facilities";
-            this.btnFacilities.UseVisualStyleBackColor = false;
-            this.btnFacilities.MouseEnter += new System.EventHandler(this.btnFacilities_MouseEnter);
-            // 
-            // btnReports
-            // 
-            this.btnReports.BackColor = System.Drawing.Color.LightSkyBlue;
-            this.btnReports.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.btnReports.FlatAppearance.BorderSize = 0;
-            this.btnReports.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnReports.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnReports.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnReports.Image = global::SportsFacilityManagementSystem.Properties.Resources.Report;
-            this.btnReports.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnReports.Location = new System.Drawing.Point(13, 388);
-            this.btnReports.Name = "btnReports";
-            this.btnReports.Size = new System.Drawing.Size(215, 76);
-            this.btnReports.TabIndex = 8;
-            this.btnReports.Text = "Reports";
-            this.btnReports.UseVisualStyleBackColor = false;
-            this.btnReports.MouseEnter += new System.EventHandler(this.btnReports_MouseEnter);
             // 
             // btnSearchMembers
             // 
@@ -284,6 +185,106 @@
             this.btnUpcoming.UseVisualStyleBackColor = false;
             this.btnUpcoming.Visible = false;
             // 
+            // pbLogo
+            // 
+            this.pbLogo.Location = new System.Drawing.Point(-1, 0);
+            this.pbLogo.Name = "pbLogo";
+            this.pbLogo.Size = new System.Drawing.Size(230, 160);
+            this.pbLogo.TabIndex = 15;
+            this.pbLogo.TabStop = false;
+            this.pbLogo.MouseEnter += new System.EventHandler(this.pbLogo_MouseEnter);
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.btnLogout.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnLogout.FlatAppearance.BorderSize = 0;
+            this.btnLogout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogout.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogout.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnLogout.Image = global::SportsFacilityManagementSystem.Properties.Resources.Logout;
+            this.btnLogout.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLogout.Location = new System.Drawing.Point(13, 467);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(215, 76);
+            this.btnLogout.TabIndex = 10;
+            this.btnLogout.Text = "Logout";
+            this.btnLogout.UseVisualStyleBackColor = false;
+            this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
+            this.btnLogout.MouseEnter += new System.EventHandler(this.btnLogout_MouseEnter);
+            // 
+            // btnBooking
+            // 
+            this.btnBooking.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.btnBooking.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnBooking.FlatAppearance.BorderSize = 0;
+            this.btnBooking.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBooking.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBooking.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnBooking.Image = global::SportsFacilityManagementSystem.Properties.Resources.Booking;
+            this.btnBooking.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnBooking.Location = new System.Drawing.Point(13, 160);
+            this.btnBooking.Name = "btnBooking";
+            this.btnBooking.Size = new System.Drawing.Size(215, 76);
+            this.btnBooking.TabIndex = 1;
+            this.btnBooking.Text = "Bookings";
+            this.btnBooking.UseVisualStyleBackColor = false;
+            this.btnBooking.MouseEnter += new System.EventHandler(this.btnBooking_MouseEnter);
+            // 
+            // btnMembers
+            // 
+            this.btnMembers.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.btnMembers.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnMembers.FlatAppearance.BorderSize = 0;
+            this.btnMembers.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMembers.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMembers.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnMembers.Image = global::SportsFacilityManagementSystem.Properties.Resources.Members;
+            this.btnMembers.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnMembers.Location = new System.Drawing.Point(13, 236);
+            this.btnMembers.Name = "btnMembers";
+            this.btnMembers.Size = new System.Drawing.Size(215, 76);
+            this.btnMembers.TabIndex = 7;
+            this.btnMembers.Text = "Members";
+            this.btnMembers.UseVisualStyleBackColor = false;
+            this.btnMembers.MouseEnter += new System.EventHandler(this.btnMembers_MouseEnter);
+            // 
+            // btnFacilities
+            // 
+            this.btnFacilities.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.btnFacilities.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnFacilities.FlatAppearance.BorderSize = 0;
+            this.btnFacilities.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFacilities.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFacilities.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnFacilities.Image = global::SportsFacilityManagementSystem.Properties.Resources.Stadium;
+            this.btnFacilities.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnFacilities.Location = new System.Drawing.Point(13, 312);
+            this.btnFacilities.Name = "btnFacilities";
+            this.btnFacilities.Size = new System.Drawing.Size(215, 76);
+            this.btnFacilities.TabIndex = 9;
+            this.btnFacilities.Text = "Facilities";
+            this.btnFacilities.UseVisualStyleBackColor = false;
+            this.btnFacilities.MouseEnter += new System.EventHandler(this.btnFacilities_MouseEnter);
+            // 
+            // btnReports
+            // 
+            this.btnReports.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.btnReports.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnReports.FlatAppearance.BorderSize = 0;
+            this.btnReports.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReports.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReports.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnReports.Image = global::SportsFacilityManagementSystem.Properties.Resources.Report;
+            this.btnReports.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnReports.Location = new System.Drawing.Point(13, 388);
+            this.btnReports.Name = "btnReports";
+            this.btnReports.Size = new System.Drawing.Size(215, 76);
+            this.btnReports.TabIndex = 8;
+            this.btnReports.Text = "Reports";
+            this.btnReports.UseVisualStyleBackColor = false;
+            this.btnReports.MouseEnter += new System.EventHandler(this.btnReports_MouseEnter);
+            // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -298,6 +299,7 @@
             this.Controls.Add(this.btnAddMembers);
             this.Controls.Add(this.btnSearchMembers);
             this.Controls.Add(this.pnlSidebar);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximumSize = new System.Drawing.Size(1000, 580);
             this.MinimumSize = new System.Drawing.Size(1000, 580);
--- SportsFacilityManagementSystem/frmLogin.Designer.cs
+++ SportsFacilityManagementSystem/frmLogin.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.btnLogin = new System.Windows.Forms.Button();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
@@ -43,19 +44,17 @@
             // 
             // pictureBox3
             // 
-            this.pictureBox3.Location = new System.Drawing.Point(128, 111);
-            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox3.Location = new System.Drawing.Point(85, 71);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(147, 136);
+            this.pictureBox3.Size = new System.Drawing.Size(98, 87);
             this.pictureBox3.TabIndex = 15;
             this.pictureBox3.TabStop = false;
             // 
             // btnLogin
             // 
-            this.btnLogin.Location = new System.Drawing.Point(345, 291);
-            this.btnLogin.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnLogin.Location = new System.Drawing.Point(230, 186);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(178, 56);
+            this.btnLogin.Size = new System.Drawing.Size(119, 36);
             this.btnLogin.TabIndex = 14;
             this.btnLogin.Text = "Login";
             this.btnLogin.UseVisualStyleBackColor = true;
@@ -63,57 +62,51 @@
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(425, 119);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox2.Location = new System.Drawing.Point(283, 76);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(34, 34);
+            this.pictureBox2.Size = new System.Drawing.Size(23, 22);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 13;
             this.pictureBox2.TabStop = false;
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(458, 201);
-            this.txtPassword.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtPassword.Location = new System.Drawing.Point(305, 129);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.PasswordChar = '*';
-            this.txtPassword.Size = new System.Drawing.Size(214, 31);
+            this.txtPassword.Size = new System.Drawing.Size(144, 22);
             this.txtPassword.TabIndex = 12;
             // 
             // txtUserID
             // 
-            this.txtUserID.Location = new System.Drawing.Point(458, 119);
-            this.txtUserID.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtUserID.Location = new System.Drawing.Point(305, 76);
             this.txtUserID.Name = "txtUserID";
-            this.txtUserID.Size = new System.Drawing.Size(214, 31);
+            this.txtUserID.Size = new System.Drawing.Size(144, 22);
             this.txtUserID.TabIndex = 11;
             // 
             // lblPassword
             // 
             this.lblPassword.AutoSize = true;
-            this.lblPassword.Location = new System.Drawing.Point(304, 204);
-            this.lblPassword.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPassword.Location = new System.Drawing.Point(203, 131);
             this.lblPassword.Name = "lblPassword";
-            this.lblPassword.Size = new System.Drawing.Size(118, 25);
+            this.lblPassword.Size = new System.Drawing.Size(77, 17);
             this.lblPassword.TabIndex = 10;
             this.lblPassword.Text = "Password :";
             // 
             // lblUserID
             // 
             this.lblUserID.AutoSize = true;
-            this.lblUserID.Location = new System.Drawing.Point(304, 122);
-            this.lblUserID.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblUserID.Location = new System.Drawing.Point(203, 78);
             this.lblUserID.Name = "lblUserID";
-            this.lblUserID.Size = new System.Drawing.Size(95, 25);
+            this.lblUserID.Size = new System.Drawing.Size(63, 17);
             this.lblUserID.TabIndex = 9;
             this.lblUserID.Text = "User ID :";
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(425, 201);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox1.Location = new System.Drawing.Point(283, 129);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(34, 34);
+            this.pictureBox1.Size = new System.Drawing.Size(23, 22);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 8;
             this.pictureBox1.TabStop = false;
@@ -121,9 +114,10 @@
             // frmLogin
             // 
             this.AcceptButton = this.btnLogin;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(533, 288);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.pictureBox2);
@@ -132,6 +126,8 @@
             this.Controls.Add(this.lblPassword);
             this.Controls.Add(this.lblUserID);
             this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "frmLogin";
             this.Text = "Login";
             this.Load += new System.EventHandler(this.frmLogin_Load);
