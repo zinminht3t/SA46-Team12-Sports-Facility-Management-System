3279017e651f392a2bda7166353a450f25b88fba
Zin Min Htet <zinminht3t@gmail.com>
2018-03-26 19:37:56 +0800

    frmlogin changed

--- SportsFacilityManagementSystem/frmLogin.cs
+++ SportsFacilityManagementSystem/frmLogin.cs
@@ -67,22 +67,23 @@ namespace SportsFacilityManagementSystem
 
         public void ChangeMemberStatus()
         {
+            SportsFacilitiesEntities cxt = new SportsFacilitiesEntities();
             List<Member> lstmember = new List<Member>();
-            lstmember = ctx.Members.Where(x => x.expirydate < DateTime.Today).ToList();
+            lstmember = cxt.Members.Where(x => x.expirydate < DateTime.Today).ToList();
             foreach (var member in lstmember)
             {
-                ctx.Members.First(x => x.memberid == member.memberid).status = "Inactive";
+                cxt.Members.First(x => x.memberid == member.memberid).status = "Inactive";
             }
 
 
             List<Member> inlstmember = new List<Member>();
-            inlstmember = ctx.Members.Where(x => x.expirydate > DateTime.Today).ToList();
+            inlstmember = cxt.Members.Where(x => x.expirydate > DateTime.Today).ToList();
             foreach (var member in lstmember)
             {
-                ctx.Members.First(x => x.memberid == member.memberid).status = "Active";
+                cxt.Members.First(x => x.memberid == member.memberid).status = "Active";
             }
 
-            ctx.SaveChanges();
+            cxt.SaveChanges();
         }
 
         public static string EncryptPassword(string password)
